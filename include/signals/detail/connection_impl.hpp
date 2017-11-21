6f4a9c7280f86a38b89b9ae4433b4cd52200b6eb
Anthony Leedom <anthonym.leedom@gmail.com>
2017-11-21 14:05:55 -0600

    Replace bind() with lambda in Connection_impl.
    Gets rid of a good amount of unneeded code.

--- include/signals/detail/connection_impl.hpp
+++ include/signals/detail/connection_impl.hpp
@@ -1,11 +1,9 @@
 #ifndef DETAIL_CONNECTION_IMPL_HPP
 #define DETAIL_CONNECTION_IMPL_HPP
+#include <signals/detail/connection_impl_base.hpp>
+#include <signals/slot.hpp>
 
-#include "connection_impl_base.hpp"
-#include "placeholder_template.hpp"
-#include "../slot.hpp"
-
-#include <functional>
+#include <memory>
 #include <utility>
 
 namespace sig {
@@ -17,28 +15,26 @@ class Connection_impl;
 // Implementation class for Connection. This class owns the Slot involved in
 // the connection. Inherits from Connection_impl_base, which implements shared
 // connection block counts.
-// The base class also provides an interface for the Connection class to hold a
-// pointer to a non-templated impl. This is required so that connection can
-// remain a non-template, while having impls that vary depending on the type of
-// Slot being connected.
 template <typename Ret, typename... Args>
 class Connection_impl<Ret(Args...)> : public Connection_impl_base {
    public:
-    using extended_slot_t = Slot<Ret(const Connection&, Args...)>;
+    using Extended_slot_t = Slot<Ret(const Connection&, Args...)>;
 
     Connection_impl() : Connection_impl_base{}, slot_{}, connected_{false} {}
 
-    explicit Connection_impl(const Slot<Ret(Args...)>& s)
-        : Connection_impl_base{}, slot_{s}, connected_{true} {}
+    explicit Connection_impl(Slot<Ret(Args...)> s)
+        : Connection_impl_base{}, slot_{std::move(s)}, connected_{true} {}
 
-    // Constructs a Connection_impl with extended_slot and connection. This
-    // binds the connection to the first parameter of the extended_slot_t
-    // function so that the signature of the function fits with the signal type.
-    // Also copies over tracked items from the extended slot into the new slot.
-    Connection_impl& emplace_extended(const extended_slot_t& es,
+    // Constructs a Connection_impl with an extended slot and connection. This
+    // binds the connection to the first parameter of the Extended_slot_t
+    // function. The slot function type then matches the signal function type.
+    // Then copies tracked items from the extended slot into the new slot.
+    Connection_impl& emplace_extended(const Extended_slot_t& es,
                                       const Connection& c) {
         connected_ = true;
-        slot_.slot_function() = bind_connection(es.slot_function(), c);
+        slot_.slot_function() = [c, es](Args&&... args) {
+            return es.slot_function()(c, std::forward<Args>(args)...);
+        };
         for (const std::weak_ptr<void>& wp : es.get_tracked_container()) {
             slot_.track(wp);
         }
@@ -54,29 +50,9 @@ class Connection_impl<Ret(Args...)> : public Connection_impl_base {
     const Slot<Ret(Args...)>& get_slot() const { return slot_; }
 
    private:
-    std::function<Ret(Args...)> bind_connection(
-        std::function<Ret(const Connection&, Args...)> f,
-        const Connection& c) {
-        return bind_connection(
-            std::forward<std::function<Ret(const Connection&, Args...)>>(f),
-            std::forward<const Connection&>(c),
-            std::index_sequence_for<Args...>{});
-    }
-
-    template <typename IntType, IntType... Is>
-    std::function<Ret(Args...)> bind_connection(
-        std::function<Ret(const Connection&, Args...)> f,
-        const Connection& c,
-        std::integer_sequence<IntType, Is...>) {
-        return std::bind(
-            std::forward<std::function<Ret(const Connection&, Args...)>>(f),
-            std::forward<const Connection&>(c), Placeholder_template<Is>{}...);
-    }
-
     Slot<Ret(Args...)> slot_;
     bool connected_;
 };
 
 }  // namespace sig
-
 #endif  // DETAIL_CONNECTION_IMPL_HPP
--- include/signals/detail/connection_impl_base.hpp
+++ include/signals/detail/connection_impl_base.hpp
@@ -1,18 +1,12 @@
 #ifndef DETAIL_CONNECTION_IMPL_BASE_HPP
 #define DETAIL_CONNECTION_IMPL_BASE_HPP
-
 #include <cstddef>
 
-// Forward Declarations
-namespace sig {
-class Connection;
-}  // namespace sig
-
 namespace sig {
 
-// Access to connections without template parameters. This is the interface for
-// the Connection_impl class template. This is used by the connnecton class as
-// a pointer with a concrete Connection_impl instantiation.
+// Provides an interface for the Connection class to hold a pointer to a
+// non-templated implementation. A Connection can remain non-templated, while
+// having an internal implementation vary on the Slot type.
 class Connection_impl_base {
    public:
     virtual ~Connection_impl_base() = default;
@@ -29,10 +23,8 @@ class Connection_impl_base {
     inline void remove_block() { --blocking_object_count_; }
 
    protected:
-    // Connections keep track of how many objects are blocking the connection.
     std::size_t blocking_object_count_ = 0;
 };
 
 }  // namespace sig
-
 #endif  // DETAIL_CONNECTION_IMPL_BASE_HPP
